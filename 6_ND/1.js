436b33b151354cbe87b5c638baba9b41724a01da
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-06-30 21:58:15 +0300

    Initial commit

---
+++ 6_ND/1.js
@@ -0,0 +1,51 @@
+"use strict"
+
+let skaicius1 = 2;
+let skaicius2 = 3;
+let skaicius3 = 4;
+let skaicius4 = 5;
+let suma = skaicius1+skaicius2+skaicius3+skaicius4;
+let sandauga = skaicius1*skaicius2*skaicius3*skaicius4;
+
+console.log(skaicius1);
+console.log(skaicius2);
+console.log(skaicius3);
+console.log(skaicius4);
+console.log(suma);
+console.log(sandauga);
+
+let temp;
+
+temp = skaicius1;
+skaicius1 = skaicius2;
+skaicius2 = temp;
+
+console.log(`${skaicius1}`);
+console.log(`${skaicius2}`);
+
+[skaicius3, skaicius4] = [skaicius4, skaicius3];
+console.log(`${skaicius3}`);
+console.log(`${skaicius4}`);
+
+alert(console.log(`${skaicius1}`));
+alert(console.log(`${skaicius2}`));
+alert(console.log(`${skaicius3}`));
+alert(console.log(`${skaicius4}`));
+
+let maxSkaicius = Number.MAX_VALUE;
+
+console.log(maxSkaicius);
+
+let skaicius5 = 6;
+let skaicius6 = 7;
+
+skaicius5 = skaicius5 ^ skaicius6;
+skaicius6 = skaicius5 ^ skaicius6;
+skaicius5 = skaicius5 ^ skaicius6;
+
+console.log(`${skaicius5}`);
+console.log(`${skaicius6}`);
+
+document.querySelector('#rezultatas').innerHTML=skaicius5 + "" + skaicius6;
+
+
---
+++ 6_ND/2.js
@@ -0,0 +1,19 @@
+document.querySelector('#sudeti').addEventListener('click', function(){
+    console.log('sudeta')
+});
+document.querySelector('#atimti').addEventListener('click', function(){
+    console.log('atimta')
+});
+document.querySelector('#sudauginti').addEventListener('click', function(){
+    console.log('sudauginta')
+});
+document.querySelector('#padalinti').addEventListener('click', function(){
+    console.log('padalinta')
+});
+
+let input1;
+let input2;
+
+input1 = parseFloat(document.querySelector('#input1').value);
+input2 = parseFloat(document.querySelector('#input2').value);
+
