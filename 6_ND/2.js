d7236e5ce3e3f3837a0e47468afd6e3f44ef5726
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-06-30 23:40:17 +0300

    2 uzduotis/skaiciuotuvas

--- 6_ND/2.js
+++ 6_ND/2.js
@@ -1,6 +1,7 @@
 document.querySelector('#sudeti').addEventListener('click', function(){
     console.log('sudeta')
 });
+
 document.querySelector('#atimti').addEventListener('click', function(){
     console.log('atimta')
 });
@@ -13,7 +14,50 @@ document.querySelector('#padalinti').addEventListener('click', function(){
 
 let input1;
 let input2;
+let rezultatas;
 
 input1 = parseFloat(document.querySelector('#input1').value);
 input2 = parseFloat(document.querySelector('#input2').value);
+
+if (document.querySelector('#sudeti').addEventListener('click', function add(){
+    input1 = parseFloat(document.querySelector('#input1').value)
+    input2 = parseFloat(document.querySelector('#input2').value)
+
+    rezultatas=input1+input2
+    console.log(rezultatas)
+    document.querySelector('#skaiciuotuvas').innerHTML=rezultatas;
+}))
+    {}
+
+if (document.querySelector('#atimti').addEventListener('click', function substract(){
+    input1 = parseFloat(document.querySelector('#input1').value)
+    input2 = parseFloat(document.querySelector('#input2').value)
+
+    rezultatas=input1-input2
+    console.log(rezultatas)
+    document.querySelector('#skaiciuotuvas').innerHTML=rezultatas;
+}))
+    {}
+
+if (document.querySelector('#sudauginti').addEventListener('click', function multiply(){
+    input1 = parseFloat(document.querySelector('#input1').value)
+    input2 = parseFloat(document.querySelector('#input2').value)
+
+    rezultatas=input1*input2
+    console.log(rezultatas)
+    document.querySelector('#skaiciuotuvas').innerHTML=rezultatas;
+}))
+    {}
+ 
+if (document.querySelector('#padalinti').addEventListener('click', function divide(){
+    input1 = parseFloat(document.querySelector('#input1').value)
+    input2 = parseFloat(document.querySelector('#input2').value)
+
+    rezultatas=input1/input2
+    console.log(rezultatas)
+    document.querySelector('#skaiciuotuvas').innerHTML=rezultatas;
+}))
+    {}
+
+
 
